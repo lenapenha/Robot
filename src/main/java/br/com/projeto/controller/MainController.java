e1dc9333f23ce520197d1b316c3614daeca9d9e8
Helena Penha <helena.penha@concrete.com.br>
2017-06-29 12:01:21 -0300

    configuracao inicial

---
+++ src/main/java/br/com/projeto/controller/MainController.java
@@ -0,0 +1,10 @@
+package br.com.projeto.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by helenapenha on 29/06/17.
+ */
+@RestController
+public class MainController {
+}
---
+++ src/main/java/br/com/projeto/robotRasp/RobotRaspApplication.java
@@ -0,0 +1,12 @@
+package br.com.projeto.robotRasp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotRaspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotRaspApplication.class, args);
+	}
+}
